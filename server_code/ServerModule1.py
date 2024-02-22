676ccb6a6c5f2e6cf4696b536ce3c238ee119c96
yeeping50@hotmail.com <yeeping50@hotmail.com>
2024-02-22 02:53:25 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,10 +1,8 @@
 import anvil.pdf
-
 from anvil.pdf import PDFRenderer
 
 @anvil.server.callable
 def create_pdf(name, amount, for1):
   pdf = anvil.pdf.render_form('Ticket', name, amount, for1)
   pdf = PDFRenderer(filename=f'{name} Receipt.pdf').render_form('Ticket', name, amount, for1)
-
   return pdf
